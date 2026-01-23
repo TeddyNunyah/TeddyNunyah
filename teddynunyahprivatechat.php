a5f7f77be6a85cc696b6ab775da74dd2ba3663fe
teddy nunyah <teddy@teddynunyah.cfd>
2026-01-22 18:44:53 -0600

    made message fetch repeat every second

--- teddynunyahprivatechat.php
+++ teddynunyahprivatechat.php
@@ -3,15 +3,18 @@
 <head>
     <title>Teddy Nunyah Private Chat</title>
     <script>
-        const request = new Request("tnpcmessages")
-        let messages;
-        window.fetch(request)
-        .then((response) => {
-            return response.text()
-        })
-        .then((text) => {
-            document.getElementById("messages").innerHTML = text;
-        })
+        (function fetchMessages(){
+            const request = new Request("tnpcmessages")
+            let messages;
+            window.fetch(request)
+            .then((response) => {
+                return response.text()
+            })
+            .then((text) => {
+                document.getElementById("messages").innerHTML = text;
+            })
+            setTimeout(fetchMessages, 1000)
+        })();
     </script>
 </head>
 <body>
