8c4b31b81cedf0f6092cb528674d26e1f91b9040
ItsJiggles <iloveoldmenandwomen@gmail.com>
2026-02-01 18:03:48 -0600

    cleaned up code, added autoscrolling if user is far enough down the page

--- tnpcmessages.php
+++ tnpcmessages.php
@@ -4,5 +4,5 @@ require_once 'dbconfig.php';
 $messages = $pdo->query('SELECT * FROM messages JOIN users ON authorid = userid');
 
 foreach ($messages as $row) {
-    echo "<b>" . htmlspecialchars($row['username']) . "</b>: " . htmlspecialchars($row['message']) . "<br/>";
+    echo "<b>" . htmlspecialchars($row['username'], ENT_NOQUOTES) . "</b>: " . htmlspecialchars($row['message'], ENT_NOQUOTES) . "<br>";
 }
